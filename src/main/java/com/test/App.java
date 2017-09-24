d9776ba2d01e7f47207501a035420c4163d8c2c3
askywhale <wgit@askywhale.com>
2017-09-24 10:28:55 +0200

    First app

---
+++ src/main/java/com/test/App.java
@@ -0,0 +1,15 @@
+package fr.dawan.test.mvn1;
+
+/**
+ * Hello world!
+ *
+ */
+public class   App
+{
+
+    public static void main( String  [] args )
+    {
+        System.out.println( "Hello World!"
+		);
+    }
+}
